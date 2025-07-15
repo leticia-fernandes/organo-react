09457bfd5698fb63f7082efa9ce5f0373f3d3687
Letícia Fernandes <leticiafs1994@hotmail.com>
2025-07-15 20:11:23 -0300

    Add Banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h1>ORGANO</h1>
+      <h2>Pessoas e times organizados em um só lugar</h2>
+    </div>
+  )
+}
+
+export default Banner
